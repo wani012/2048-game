adb6be898fc6c3f1819c18a65ff2673377ea8936
wani012 <162290871+wani012@users.noreply.github.com>
2026-09-11 14:56:35 +0530

    Add Blitz 60s mode, Daily Challenge, Hammer and Shuffle Power-ups, and PWA Mobile App install

---
+++ sw.js
@@ -0,0 +1,12 @@
+﻿self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('1024-v1').then(cache => {
+      return cache.addAll(['./', 'index.html', 'manifest.json']);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
